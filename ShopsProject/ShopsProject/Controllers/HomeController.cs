36dfffb01e6d62329c5b76b45481fbb1714cb262
unknown <elkadiri.wd@gmail.com>
2019-05-14 00:26:52 +0000

    Nearby shops update

--- ShopsProject/ShopsProject/Controllers/HomeController.cs
+++ ShopsProject/ShopsProject/Controllers/HomeController.cs
@@ -39,20 +39,23 @@ namespace ShopsProject.Controllers
                 double latitude = Convert.ToDouble(Session["latitude"]);
                 double longitude = Convert.ToDouble(Session["longitude"]);
                 var currentCoord = new GeoCoordinate(latitude, longitude);
-
+                
                 var shopsList = (from sh in shopsContext.shops
                                  where !shopsContext.UserShop.Any(us => us.shop_id == sh.id && us.login_id==loginId)
                                  select sh).ToList<shop>();
-                
-                var likedShops = (from sh in shopsContext.shops                                  
-                                  join us in shopsContext.UserShop on sh.id equals us.shop_id
-                                  where us.isPreferred == false && us.login_id == loginId                                  
-                                  select sh).ToList<shop>();
 
-                foreach (var likedShop in likedShops)
+                var usersShops = (from us in shopsContext.UserShop
+                                  join sh in shopsContext.shops on us.shop_id equals sh.id
+                                  where us.login_id == loginId                        
+                                  select us).ToList<UserShop>();
+
+                foreach (var userShop in usersShops)
                 {
-                    if (shopsContext.shops.Any(a => a.id == likedShop.id))
-                        shopsList.Add(likedShop);
+                    DateTime dislikedDate = userShop.dislikedDate ?? DateTime.Now;
+                    if (userShop.login_id == loginId && userShop.isPreferred == false && userShop.isDisliked == false)
+                        shopsList.Add(shopsContext.shops.Where(a => a.id == userShop.shop_id).SingleOrDefault());
+                    if (userShop.login_id == loginId && userShop.isDisliked == true && DateTime.Now.Subtract(dislikedDate) < new TimeSpan(2, 0, 0))
+                        shopsList.Add(shopsContext.shops.Where(a => a.id == userShop.shop_id).SingleOrDefault());
                 }
                 
                 for (int i = 0; i < shopsList.Count; i++)
@@ -116,6 +119,8 @@ namespace ShopsProject.Controllers
             else if (flag == false)
             {
                 shopResult.isPreferred = false;
+                shopResult.isDisliked = true;
+                shopResult.dislikedDate = DateTime.Now;
                 shopsContext.SaveChanges();
             }
 
